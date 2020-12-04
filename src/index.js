290e1b7e4b7b54166931ae0ed5adcc56bbff9fb8
wakkki- <onsinhutuu@gmail.com>
2020-12-04 22:39:08 +0900

    ES6記述

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import "./styles.css";
+/** const,let  */
+var val1 = "var変数";
+console.log(val1);
+let val2 = "変数";
+console.log(val2);
+const val4 = {
+  name: "aa",
+  age: 28
+};
+val4.name = "uuu";
+console.log(val4);
+/** アロー関数  */
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(3, 4)); /*7が出力 */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/** 分割代入 とテンプレート文字列 */
+const profile = {
+  name: "aaa",
+  age: 22
+};
+const message1 = `私の名前は${profile.name}です。年齢は${profile.age}`;
+console.log(message1);
+/** profileを何回も書かなくて済む  */
+const { name, age } = profile;
+const message2 = `私の名前は${name}です。年齢は${age}`;
+console.log(message2);
+
+/** スプレッド構文  */
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [9, 0, 7, 6, 8];
+/** 配列の残り  */
+function unshift(array, ...rest) {
+  return [...array, ...rest];
+}
+console.log(unshift(arr1, 6, 7, 8, 9));
+/** 配列をコピー */
+const arr3 = [...arr1];
+console.log(arr3);
+/** 配列を結合 */
+const arr4 = [...arr1, ...arr2];
+console.log(arr4);
